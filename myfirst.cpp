0d046429c57bbda3af4977b7b118e52bebe758e1
anji <anji_cruz@msn.com>
2023-08-17 12:00:14 +0800

    InitialCommit 8/17 12pm

---
+++ myfirst.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "Come up and C++ me sometime.";
+    cout << endl;
+    cout << "You will not regret it." << endl;
+    return 0;
+}
