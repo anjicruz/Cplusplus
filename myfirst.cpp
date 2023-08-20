befb36ca7a89be919504d1458f3a70081d9ce482
anji <anji_cruz@msn.com>
2023-08-20 16:18:16 +0800

    comments 8/20

--- myfirst.cpp
+++ myfirst.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     using namespace std;
-    cout << "Come up and C++ me sometime.";
+    cout << "Come up and C++ me sometime."; // argument
     cout << endl;
     cout << "You will not regret it." << endl;
     return 0;
