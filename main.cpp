c25235e5378ba9071be8a943cf59f5fb90d06df1
anji <anji_cruz@msn.com>
2023-08-22 08:49:19 +0800

    switching values 8/22

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 int main() {
-    int file_size = 0;
-    double sales = 9.99;
-    std::cout << file_size;
+    int a = 1;
+    int b = 2;
+    int temp = a;
+    a = b;
+    b = temp;
+    std::cout << a;
     return 0;
 }
