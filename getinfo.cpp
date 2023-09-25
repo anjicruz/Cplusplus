f6432511d4e450d7c1168f22ee727db9701fd8d9
anji <anji_cruz@msn.com>
2023-09-26 07:18:58 +0800

    getinfo carrots 9/26

--- getinfo.cpp
+++ getinfo.cpp
@@ -3,6 +3,14 @@
 
 int main()
 {
+    using namespace std;
+    int carrots;
+    cout << "How many carrots do you have?" << endl;
+    cin >> carrots;
+    cout << "Here are 2 more. ";
+    carrots = carrots + 2;
+    cout << "Now you have " << carrots << " carrots." << endl;
+
     return 0; 
 }
 
