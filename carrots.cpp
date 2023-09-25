321a8a1296d71987adc24119a438eb76cf50d85c
anji <anji_cruz@msn.com>
2023-09-26 05:18:17 +0800

    carrots refomatted 9/26

--- carrots.cpp
+++ carrots.cpp
@@ -7,10 +7,7 @@ int main()
     int carrots;
 
     carrots = 25;
-    cout << "I have ";
-    cout << carrots;
-    cout << " carrots";
-    cout << endl;
+    cout << "I have " << carrots << " carrots" << endl;
     carrots = carrots - 1;
     cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
     return 0; 
