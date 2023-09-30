cef12b68a344f746501133a0cd7bba367f21e295
anji <anji_cruz@msn.com>
2023-09-30 17:17:18 +0800

    std namespace available to both functions 9/30

--- ourfunc.cpp
+++ ourfunc.cpp
@@ -1,10 +1,11 @@
 // ourfunc.cpp -- defining you own functions
 #include <iostream>
+using namespace std; // affects all function definitions in this file
+
 void simon(int); // function prototype for simon()
 
 int main () 
 {
-    using namespace std;
     simon(3); // call the simon() function
     cout << "Pick an integer ";
     int count;
@@ -15,7 +16,7 @@ int main ()
 }
 void simon(int n) // define the simon() function
 {
-    using namespace std;
+    // using namespace std;
     cout << "Simon says touch your toes " << n << " times." << endl; 
             // void functions do not need to return a statement
 }
