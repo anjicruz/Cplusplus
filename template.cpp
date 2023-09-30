3424a0c4ad7a6493118df583b8625a1e96d91689
anji <anji_cruz@msn.com>
2023-09-30 17:13:01 +0800

    2nd version (new variable) 9/30

--- template.cpp
+++ template.cpp
@@ -7,11 +7,12 @@ int main()
     cout << "Enter the weight in stone: ";
     cin >> stone;
     int pounds = stonetolb(stone);
-    cout << stone << " stone: ";
+    cout << stone << " stone is equal to ";
     cout << pounds << " pounds." << endl;
     return 0; 
 }
 int stonetolb(int sts)
 {
-    return 14 * sts;
+    int pounds = 14 * sts;
+    return pounds;
 }
