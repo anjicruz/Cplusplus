5cfc1062c7e98adf0067fe369257cd7ecc3e8f7c
anji <anji_cruz@msn.com>
2023-10-11 21:21:25 +0800

    stepfunction 10/11

---
+++ step_function.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int step_function(int x)
+{
+    int result = 0;
+    if (x<0) {
+        result = -1;
+    } else if (x>0) {
+        result = 1;
+    }
+    return result; 
+}
