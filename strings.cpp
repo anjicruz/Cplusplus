7610dc246a01dfa7f15c147586b0bf037a3f6400
anji <anji_cruz@msn.com>
2023-10-12 00:08:30 +0800

    storing strings in an array 10/12

---
+++ strings.cpp
@@ -0,0 +1,21 @@
+// strings.cpp -- storing strings in an array
+#include <iostream>
+#include <cstring>      // for strlen function
+int main()
+{
+    // using namespace std;
+    const int Size = 15;
+    char name1[Size];               // empty array
+    char name2[Size] = "C++owboy";  // initialized array
+
+    std::cout << "Howdy! I'm " << name2;
+    std::cout << " What's your name?\n";
+    std::cin >> name1;
+    std::cout << "Well, " << name1 << ", your name has ";
+    std::cout << strlen(name1) << " letters and is stored"<< " in an array of " << sizeof(name1) << " bytes.\n";
+    std::cout << "Your initial is " << name1[0] << ".\n";
+    name2[3] = '\0';                // set to null character
+    std::cout << "Here are the first 3 characters of my name: ";
+    std::cout << name2 << std::endl;
+    return 0; 
+}
